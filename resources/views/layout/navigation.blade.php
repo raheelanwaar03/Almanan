e83d0b9ae7f326a9171bf72d95431bfc35d95984
Raheel Anwaar <raheelanwaar0316@gmail.com>
2022-12-26 23:34:47 +0500

    spelling correction

--- resources/views/layout/navigation.blade.php
+++ resources/views/layout/navigation.blade.php
@@ -54,7 +54,7 @@
                                                 class="transition-all hover:text-secondary">About</a>
                                         </li>
                                         <li class="mr-7 xl:mr-[40px] relative group py-[20px]">
-                                            <a href="#" class="transition-all hover:text-secondary">Projests</a>
+                                            <a href="#" class="transition-all hover:text-secondary">Projects</a>
                                             <ul
                                                 class="list-none bg-white drop-shadow-[0px_6px_10px_rgba(0,0,0,0.2)] rounded-[12px] flex flex-wrap flex-col w-[220px] absolute top-[120%] left-1/2 -translate-x-1/2 transition-all group-hover:top-[100%] invisible group-hover:visible opacity-0 group-hover:opacity-100">
                                                 <li
@@ -97,7 +97,7 @@
                                         </li>
                                         <li class="mr-7 xl:mr-[40px] relative group py-[20px]">
                                             <a href="{{ url('/Services') }}"
-                                                class="transition-all hover:text-secondary">Service's</a>
+                                                class="transition-all hover:text-secondary">Services</a>
                                         </li>
                                         <li class="mr-7 xl:mr-[40px] relative group py-[20px]">
                                             <a href="{{ url('/Contact-Us') }}"
@@ -159,7 +159,7 @@
                                 <li
                                     class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
                                     <a href="{{ url('/SpanishVilla') }}"
-                                        class="font-lora leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-primary hover:text-white text-center my-[-1px] rounded-t-[12px]">Spanish
+                                        class="font-lora leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-primary hover:text-white text-center my-[-1px] rounded-t-[12px]">AlMannan Spanish
                                         Villa</a>
                                 </li>
                                 <li
