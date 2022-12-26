3b8029742ab0508d8114aeaa8a36b26e35b86b79
Raheel Anwaar <raheelanwaar0316@gmail.com>
2022-12-26 23:46:00 +0500

    s added

--- resources/views/layout/navigation.blade.php
+++ resources/views/layout/navigation.blade.php
@@ -67,7 +67,7 @@
                                                     class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
                                                     <a href="{{ url('/SpanishVilla') }}"
                                                         class="font-lora leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-primary hover:text-white text-center my-[-1px] rounded-t-[12px]">Almannan Spanish
-                                                        Villa</a>
+                                                        Villas</a>
                                                 </li>
                                                 <li
                                                     class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
@@ -160,7 +160,7 @@
                                     class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
                                     <a href="{{ url('/SpanishVilla') }}"
                                         class="font-lora leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-primary hover:text-white text-center my-[-1px] rounded-t-[12px]">AlMannan Spanish
-                                        Villa</a>
+                                        Villas</a>
                                 </li>
                                 <li
                                     class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
