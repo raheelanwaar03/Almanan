49f20a855de83db12ee6711ff871bc15ebea48f1
Raheel Anwaar <raheelanwaar0316@gmail.com>
2022-12-26 17:32:59 +0500

    resposiveness correction

--- resources/views/layout/navigation.blade.php
+++ resources/views/layout/navigation.blade.php
@@ -32,32 +32,31 @@
         <!-- for Navigation -->
         <!-- Header start -->
 
-
-        <header id="sticky-header" class="absolute left-0 top-[15px] lg:top-[30px] xl:top-[45px] w-full z-10">
-            <div class="container">
-                <div class="grid grid-cols-12">
-                    <div class="col-span-12">
-                        <div class="flex flex-wrap items-center justify-between">
-                            <a href="{{ url('/') }}" class="block">
-                                <img class="w-full h-full" src="assets/images/logo/Almanan1.png" loading="lazy"
-                                    width="99" height="46" alt="brand logo">
-                            </a>
-                            <nav class="flex flex-wrap items-center">
-                                <ul
-                                    class="hidden lg:flex flex-wrap items-center font-lora text-[16px] xl:text-[18px] leading-none text-black">
-
+        <div class="min-h-[100px]">
+            <header id="sticky-header" class="relative bg-[#E8F1FF] lg:py-[30px] z-[25] secondary-sticky">
+                <div class="container">
+                    <div class="grid grid-cols-12">
+                        <div class="col-span-12">
+                            <div class="flex flex-wrap items-center justify-between">
+                                <a href="index.html" class="block">
+                                    <img class="w-full h-full" src="assets/images/logo/Almanan1.png" loading="lazy"
+                                        width="99" height="46" alt="brand logo">
+                                </a>
+                                <nav class="flex flex-wrap items-center">
                                     <ul
                                         class="hidden lg:flex flex-wrap items-center font-lora text-[16px] xl:text-[18px] leading-none text-black">
                                         <li class="mr-7 xl:mr-[40px] relative group py-[20px]">
                                             <a href="{{ url('/') }}"
-                                                class="sticky-dark transition-all text-white hover:text-secondary">Home</a>
+                                                class="transition-all hover:text-secondary">Home</a>
                                         </li>
                                         <li class="mr-7 xl:mr-[40px] relative group py-[20px]">
-                                            <a href="#"
-                                                class="sticky-dark transition-all text-white hover:text-secondary">Projects</a>
-
+                                            <a href="{{ url('/About-Us') }}"
+                                                class="transition-all hover:text-secondary">About</a>
+                                        </li>
+                                        <li class="mr-7 xl:mr-[40px] relative group py-[20px]">
+                                            <a href="#" class="transition-all hover:text-secondary">Projests</a>
                                             <ul
-                                                class="list-none z-[99] bg-white drop-shadow-[0px_6px_10px_rgba(0,0,0,0.2)] rounded-[12px] flex flex-wrap flex-col w-[220px] absolute top-[120%] left-1/2 -translate-x-1/2 transition-all group-hover:top-[100%] invisible group-hover:visible opacity-0 group-hover:opacity-100">
+                                                class="list-none bg-white drop-shadow-[0px_6px_10px_rgba(0,0,0,0.2)] rounded-[12px] flex flex-wrap flex-col w-[220px] absolute top-[120%] left-1/2 -translate-x-1/2 transition-all group-hover:top-[100%] invisible group-hover:visible opacity-0 group-hover:opacity-100">
                                                 <li
                                                     class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
                                                     <a href="{{ url('/LaVistaHomes') }}"
@@ -88,42 +87,43 @@
                                                         class="font-lora leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-primary hover:text-white text-center my-[-1px] rounded-t-[12px]">AlMannan
                                                         Garden</a>
                                                 </li>
-                                                {{-- <li
-                                                    class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
-                                                    <a href="{{ url('/GreenLand') }}"
-                                                        class="font-lora leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-primary hover:text-white text-center my-[-1px] rounded-t-[12px]">Green
-                                                        Vills</a>
-                                                </li> --}}
-                                                 <li
+                                                <li
                                                     class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
                                                     <a href="{{ url('/CityPhaseI-II') }}"
                                                         class="font-lora leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-primary hover:text-white text-center my-[-1px] rounded-t-[12px]">Green
-                                                        City  I-II</a>
+                                                        City I-II</a>
                                                 </li>
+                                            </ul>
+                                        </li>
+                                        <li class="mr-7 xl:mr-[40px] relative group py-[20px]">
+                                            <a href="{{ url('/Services') }}"
+                                                class="transition-all hover:text-secondary">Service's</a>
+                                        </li>
+                                        <li class="mr-7 xl:mr-[40px] relative group py-[20px]">
+                                            <a href="{{ url('/Contact-Us') }}"
+                                                class="transition-all hover:text-secondary">Contact</a>
                                         </li>
                                     </ul>
                                     </li>
-                                    <li class="mr-7 xl:mr-[40px] relative group py-[20px]">
-                                        <a href="{{ url('/About-Us') }}"
-                                            class="sticky-dark transition-all text-white hover:text-secondary">About</a>
-                                    </li>
-                                    <li class="mr-7 xl:mr-[40px] relative group py-[20px]">
-                                        <a href="{{ url('/Services') }}"
-                                            class="sticky-dark transition-all text-white hover:text-secondary">Services</a>
-                                    </li>
-                                    <li class="mr-7 xl:mr-[40px] relative group py-[20px]">
-                                        <a href="{{ url('/Contact-Us') }}"
-                                            class="sticky-dark transition-all text-white hover:text-secondary">Contact</a>
-                                    </li>
-                                </ul>
-
-                                </ul>
-                            </nav>
+                                    <ul class="flex flex-wrap items-center">
+                                        <li class="ml-2 sm:ml-5 lg:hidden">
+                                            <a href="#offcanvas-mobile-menu"
+                                                class="offcanvas-toggle flex text-[#016450] hover:text-secondary">
+                                                <svg width="24" height="24" class="fill-current"
+                                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                                    <path
+                                                        d="M0 96C0 78.33 14.33 64 32 64H416C433.7 64 448 78.33 448 96C448 113.7 433.7 128 416 128H32C14.33 128 0 113.7 0 96zM0 256C0 238.3 14.33 224 32 224H416C433.7 224 448 238.3 448 256C448 273.7 433.7 288 416 288H32C14.33 288 0 273.7 0 256zM416 448H32C14.33 448 0 433.7 0 416C0 398.3 14.33 384 32 384H416C433.7 384 448 398.3 448 416C448 433.7 433.7 448 416 448z" />
+                                                </svg>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </header>
+            </header>
+        </div>
         <!-- offcanvas-overlay start -->
         <div class="offcanvas-overlay hidden fixed inset-0 bg-black opacity-50 z-50"></div>
         <!-- offcanvas-overlay end -->
@@ -146,25 +146,65 @@
                             <a href="{{ url('/') }}"
                                 class="block capitalize font-normal text-black hover:text-secondary text-base my-2 py-1 px-5">Home</a>
                         </li>
-                        <li class="relative block border-b-primary border-b first:border-t first:border-t-primary">
-                            <a href="{{ url('/Projects') }}"
-                                class="block capitalize font-normal text-black hover:text-secondary text-base my-2 py-1 px-5">Home</a>
+                        <li class="relative block border-b-primary border-b"><a href="#"
+                                class="relative block capitalize font-normal text-black hover:text-secondary text-base my-2 py-1 px-5">Projects</a>
+
+                            <ul class="offcanvas-submenu static top-auto hidden w-full visible opacity-100 capitalize">
+                                <li
+                                    class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
+                                    <a href="{{ url('/LaVistaHomes') }}"
+                                        class="font-lora leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-primary hover:text-white text-center my-[-1px] rounded-t-[12px]">La
+                                        Vista Homes</a>
+                                </li>
+                                <li
+                                    class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
+                                    <a href="{{ url('/SpanishVilla') }}"
+                                        class="font-lora leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-primary hover:text-white text-center my-[-1px] rounded-t-[12px]">Spanish
+                                        Villa</a>
+                                </li>
+                                <li
+                                    class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
+                                    <a href="{{ url('/LebertyMarket') }}"
+                                        class="font-lora leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-primary hover:text-white text-center my-[-1px] rounded-t-[12px]">Liberty
+                                        Market</a>
+                                </li>
+                                <li
+                                    class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
+                                    <a href="{{ url('/AlMannanCity') }}"
+                                        class="font-lora leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-primary hover:text-white text-center my-[-1px] rounded-t-[12px]">AlMannan
+                                        City</a>
+                                </li>
+                                <li
+                                    class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
+                                    <a href="{{ url('/GreenCity') }}"
+                                        class="font-lora leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-primary hover:text-white text-center my-[-1px] rounded-t-[12px]">AlMannan
+                                        Garden</a>
+                                </li>
+                                <li
+                                    class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
+                                    <a href="{{ url('/CityPhaseI-II') }}"
+                                        class="font-lora leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-primary hover:text-white text-center my-[-1px] rounded-t-[12px]">Green
+                                        City I-II</a>
+                                </li>
+                            </ul>
                         </li>
-                        <li class="relative block border-b-primary border-b">
-                            <a href="{{ url('/About-Us') }}"
-                                class="block capitalize font-normal text-black hover:text-secondary text-base my-2 py-1 px-5">About</a>
-                        </li>
-                        <li class="relative block border-b-primary border-b">
-                            <a href="{{ url('/Services') }}"
-                                class="block capitalize font-normal text-black hover:text-secondary text-base my-2 py-1 px-5">Services</a>
-                        </li>
-                    </ul>
+                        <li class="relative block border-b-primary border-b"><a href="{{ url('/Services') }}"
+                            class="relative block capitalize text-black hover:text-secondary text-base my-2 py-1 px-5">Services</a>
                     </li>
-                    <li class="relative block border-b-primary border-b"><a href="{{ url('/Contact-Us') }}"
-                            class="relative block capitalize text-black hover:text-secondary text-base my-2 py-1 px-5">Contact</a>
-                    </li>
+                        <li class="relative block border-b-primary border-b"><a href="{{ url('/Contact-Us') }}"
+                                class="relative block capitalize text-black hover:text-secondary text-base my-2 py-1 px-5">Contact</a>
+                        </li>
                     </ul>
                 </nav>
                 <!-- offcanvas-menu end -->
+
+                <div class="px-5 flex flex-wrap mt-3 sm:hidden">
+                    <a href="#"
+                        class="before:rounded-md before:block before:absolute before:left-auto before:right-0 before:inset-y-0 before:-z-[1] before:bg-secondary before:w-0 hover:before:w-full hover:before:left-0 hover:before:right-auto before:transition-all leading-none px-[20px] py-[15px] capitalize font-medium text-white text-[14px] xl:text-[16px] relative after:block after:absolute after:inset-0 after:-z-[2] after:bg-primary after:rounded-md after:transition-all">Add
+                        Property</a>
+                </div>
+
+
+
             </div>
         </div>
