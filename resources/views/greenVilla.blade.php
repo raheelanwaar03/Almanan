8d0e7facf94e8c669d1acd9e509640886162718f
Raheel Anwaar <raheelanwaar0316@gmail.com>
2022-12-27 00:09:39 +0500

    img updated

--- resources/views/greenVilla.blade.php
+++ resources/views/greenVilla.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <section
         class="bg-no-repeat bg-center bg-cover bg-[#FFF6F0] h-[350px] lg:h-[513px] flex flex-wrap items-center relative before:absolute before:inset-0 before:content-[''] before:bg-[#000000] before:opacity-[70%]"
-        style="background-image: url('assets/images/properties/modernCity.jpg');">
+        style="background-image: url('assets/images/properties/garder.jpg');">
         <div class="container">
             <div class="grid grid-cols-12">
                 <div class="col-span-12">
